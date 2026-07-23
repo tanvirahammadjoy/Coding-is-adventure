75e5a9c676c890f10dd370e4b1609b6c12be62bc
Tanvir Ahammad <tanvirahammadjoy@gmail.com>
2026-07-23 12:27:40 +0800

    feat: enhance chapter 4 with additional examples using cmath, decimal, fractions, random, statistics, numbers, and operator modules

--- 04-python-learning/chapter_4.py
+++ 04-python-learning/chapter_4.py
@@ -1,4 +1,11 @@
+import cmath
+import decimal
+import fractions
 import math
+import numbers
+import operator
+import random
+import statistics
 import sys
 
 # real number/float/floating point/decimal/double/double precision
@@ -31,4 +38,39 @@ print(
     f"Size of float number in bytes using math module: {math.ceil(math.log2(sys.getsizeof(total_cost)))}"
 )  # 4
 
-print(f"float information: {sys.float_info}")  # float information: sys.float_info(max=1.7976931348623157e+308, max_exp=1024, max_10_exp=308, min=2.2250738585072014e-308, min_exp=-1021, min_10_exp=-307, dig=15, mant_dig=53, epsilon=2.220
+print(
+    f"float information: {sys.float_info}"
+)  # float information: sys.float_info(max=1.7976931348623157e+308, max_exp=1024, max_10_exp=308, min=2.2250738585072014e-308, min_exp=-1021, min_10_exp=-307, dig=15, mant_dig=53, epsilon=2.220
+
+# lets see what can we do with the decimal module
+price_of_tea = decimal.Decimal("2.50")
+number_of_tea_bags = decimal.Decimal("3")
+total_cost = price_of_tea * number_of_tea_bags
+print(f"Total cost: ${total_cost}")
+
+# lets see what can we do with the fractions module
+fraction1 = fractions.Fraction(1, 2)
+fraction2 = fractions.Fraction(3, 4)
+print(fraction1 + fraction2)  # 5/4
+
+# lets see what can we do with the random module
+random_number = random.randint(1, 10)
+print(random_number)  # 5
+
+# lets see what can we do with the statistics module
+data = [1, 2, 3, 4, 5]
+mean = statistics.mean(data)
+print(mean)  # 3.0
+
+# lets see what can we do with the cmath module
+complex_number = cmath.rect(2, 3)
+print(complex_number)  # (2+3j)
+
+# lets see what can we do with the numbers module
+print(f"Number of types in numbers module: {len(numbers.__dict__)}")
+
+# lets see what can we do with the operator module
+print(f"Addition using operator module: {operator.add(2, 3)}")
+print(f"Subtraction using operator module: {operator.sub(5, 3)}")
+print(f"Multiplication using operator module: {operator.mul(2, 3)}")
+print(f"Division using operator module: {operator.truediv(6, 3)}")
