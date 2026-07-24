4e36f29dac1705974fb58d738928d09e3f68064b
Tanvir Ahammad <tanvirahammadjoy@gmail.com>
2026-07-24 16:50:38 +0800

    feat: add basic loop examples in Python including while, for, and nested loops

---
+++ 07_pythons_projects/loops_basics.py
@@ -0,0 +1,16 @@
+# lets explore loops in python
+
+# while loop in python
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+# for loop in python
+for i in range(1, 6):
+    print(i)
+
+# nested loop in python
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(i, j)
