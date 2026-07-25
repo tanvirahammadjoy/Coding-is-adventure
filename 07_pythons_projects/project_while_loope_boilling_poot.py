71710c69a5c4aa5a40948069ee3d00b528b6f5fa
Tanvir Ahammad <tanvirahammadjoy@gmail.com>
2026-07-25 23:30:13 +0800

    feat: add simulation of boiling pot with temperature increments

---
+++ 07_pythons_projects/project_while_loope_boilling_poot.py
@@ -0,0 +1,15 @@
+# we want to simulate a boiling pot
+# we want to print the temperature every 5 seconds
+# we want to start the pot at 30 degrees
+# we want to stop the pot when the temperature is 100 degrees
+
+# first lets have a temperature
+temperature = 30
+
+# now lets print the temperature
+while temperature < 100:
+    print(f"The temperature is {temperature} degrees")
+    temperature += 5
+
+# now lets print the temperature
+print(f"The temperature is {temperature} degrees")
